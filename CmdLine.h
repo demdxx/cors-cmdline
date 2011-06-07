8da58112265262acdac75fca1baaca9aabc6802b
Aguacero <demdxx@gmail.com>
2011-06-07 16:16:40 +0400

    Version 0.1

--- CmdLine.h
+++ CmdLine.h
@@ -5,7 +5,7 @@
  * Author:
  *       Dmitry Ponomarev (Aguacero) <demdxx@gmail.com>
  * 
- * Copyright (c) 2011 Dmitry Ponomarev V.}
+ * Copyright (c) 2011 Dmitry Ponomarev V.
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
